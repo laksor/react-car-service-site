b1b8518f624b36751f707fc47d85b9e658f00d64
laskor <al.moksud@gmail.com>
2022-04-25 02:49:56 -0400

    service style added

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,15 +1,15 @@
 import React from "react";
+import './Service.css';
 
 const Service = ({ service }) => {
   const { name, img, price, description } = service;
   return (
-    <div className="">
+    <div className="service">
       <img src={img} alt='kaj'></img>
       <h2>{name}</h2>
       <p>Price: {price}</p>
       <p><small>{description}</small></p>
-      <button>Book</button>
-      
+      <button>Book {name}</button>
     </div>
   );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
+import './Services.css';
 
 const Services = () => {
     const [services, setServices] = useState([]);
@@ -11,8 +12,8 @@ const Services = () => {
     }, [])
     return (
         <div>
-            <h2>Our Services</h2>
-            <div className=''>
+            <h1 className='services-title'>Our Services</h1>
+            <div className='services-container'>
             {
                 services.map(service => <Service
                     key={service.id}
