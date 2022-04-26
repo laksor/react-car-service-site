5b5065784121cdb09f67ca6c853ef2bc06fd30a0
laskor <al.moksud@gmail.com>
2022-04-26 03:23:27 -0400

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
-        <footer>
-        <p>copyright @ 2022</p>
+        <footer className='text-center mt-5'>
+        <p><small>copyright @ {year}</small></p>
         </footer>
     );
 };
