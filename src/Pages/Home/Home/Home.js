cb215badc77281cbcb36f32af02229e248af9137
laskor <al.moksud@gmail.com>
2022-04-26 03:06:57 -0400

    banner added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Experts from '../Experts/Experts';
-import Services from '../Services/Services';
+import React from "react";
+import Banner from "../Banner/Banner";
+import Experts from "../Experts/Experts";
+import Services from "../Services/Services";
 
 const Home = () => {
-    return (
-        <div>
-            <Services></Services>
-            <Experts></Experts>
-        </div>
-    );
+  return (
+    <>
+      <Banner></Banner>
+      <Services></Services>
+      <Experts></Experts>
+    </>
+  );
 };
 
 export default Home;
