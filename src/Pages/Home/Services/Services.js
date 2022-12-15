c3f06f66267b2bcb94150a7ec855f2e7aed1aa67
laskor <al.moksud@gmail.com>
2022-12-15 12:43:18 -0500

    server deployed

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -6,7 +6,7 @@ const Services = () => {
     const [services, setServices] = useState([]);
 
     useEffect( () =>{
-        fetch('http://localhost:5000/service')
+        fetch('https://car-service-sserver.onrender.com/service')
         .then(res => res.json())
         .then(data => setServices(data))
     }, [])
